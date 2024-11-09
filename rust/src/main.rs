b452fd345750629db14d222d9c2910542f20445a
chrispaig3 <v0idmatr1x.0@gmail.com>
2024-11-08 22:12:00 -0600

    add: rust boilerplate

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,21 @@
+use raylib::prelude::*;
+
+const SCREEN_X: i32 = 1920;
+const SCREEN_Y: i32 = 1080;
+
+static TITLE: &'static str = "Box Man";
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(SCREEN_X, SCREEN_Y)
+        .title(TITLE)
+        .vsync()
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
